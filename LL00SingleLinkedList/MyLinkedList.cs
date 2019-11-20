b118ba00bf9ae5b8acd9ecfbb24d01f6b6f59195
Michal Stehlík <st@pslib.cloud>
2019-11-20 09:28:14 +0100

    Přidejte soubory projektu.

---
+++ LL00SingleLinkedList/MyLinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL00SingleLinkedList
+{
+    class MyLinkedList<T>
+    {
+        public ListNode<T> head;
+
+        public void Add(T item)
+        {
+            ListNode<T> node = new ListNode<T>(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                ListNode<T> current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = node;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                int counter = 0;
+                ListNode<T> current = head;
+                while (current != null)
+                {
+                    counter++;
+                    current = current.Next;
+                }
+                return counter;
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+        }
+
+        public void AddToStart(T item)
+        {
+            ListNode<T> node = new ListNode<T>(item);
+            node.Next = head;
+            head = node;
+        }
+
+        public bool Contains(T item)
+        {
+            ListNode<T> current = head;
+            while (current != null)
+            {
+                if (current.value.Equals(item))
+                {
+                    return true;
+                }   
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int IndexOf(T item)
+        // DU
+        {
+            return -1;
+        }
+
+        public T GetItem(int index)
+        // DU
+        {
+            // throw IndexOutOfRangeException
+            return default(T);
+        }
+
+        public void Insert(int index, T item)
+        // DU
+        {
+
+        }
+
+        public void Remove(T item)
+        // SDU
+        {
+
+        }
+
+        public void RemoveAt(int index)
+        // SDU
+        {
+
+        }
+    }
+}
---
+++ LL00SingleLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LL00SingleLinkedList
+{
+    class Program
+    {
+        static public void WriteList(MyLinkedList<int> ll)
+        {
+            ListNode<int> current = ll.head;
+            while(current != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.Next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> ll = new MyLinkedList<int>();
+            ll.Add(1);
+            ll.Add(2);
+            ll.Add(3);
+            ll.AddToStart(4);
+            WriteList(ll);
+            Console.WriteLine("=" + ll.Count);
+            Console.WriteLine(ll.Contains(3));
+            Console.WriteLine(ll.Contains(99));
+            Console.ReadKey();
+        }
+    }
+}
