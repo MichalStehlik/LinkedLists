13fe142348c1967a1634b1fa3caf765cf7857a31
Michal Stehlík <st@pslib.cloud>
2019-12-18 09:00:32 +0100

    Depth, Count

--- LL01BinaryTree/Program.cs
+++ LL01BinaryTree/Program.cs
@@ -11,9 +11,15 @@ namespace LL01BinaryTree
             tree.Add(new DictionaryTerm("kočka", "cat"));
             tree.Add(new DictionaryTerm("myš", "mouse"));
             tree.Add(new DictionaryTerm("kůň", "horse"));
+            tree.Add(new DictionaryTerm("kráva", "cow"));
+            tree.Add(new DictionaryTerm("antilopa", "antelope"));
+            tree.Add(new DictionaryTerm("bizon", "bison"));
+            tree.Add(new DictionaryTerm("žirafa", "giraffe"));
             TreeNode<DictionaryTerm> found = tree.Find(new DictionaryTerm("kočka", "swamp monster"));
             if (found != null) Console.WriteLine(found.value.English);
             Console.WriteLine(tree);
+            Console.WriteLine(tree.Depth());
+            Console.WriteLine(tree.Count());
             Console.ReadKey();
         }
     }
